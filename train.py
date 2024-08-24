a1d0eba2a66c68d959cbf22583092e9394dce5be
michaelacn <michaelacn20@gmail.com>
2024-08-24 14:28:16 +0200

    update train.py

--- train.py
+++ train.py
@@ -12,12 +12,7 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 
 
-if torch.cuda.is_available():
-    device = "cuda" # Use NVIDIA GPU (if available)
-elif torch.backends.mps.is_available():
-    device = "mps" # Use Apple Silicon GPU (if available)
-else:
-    device = "cpu" # Default to CPU if no GPU is available
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def parse_args():
@@ -40,7 +35,7 @@ def get_model(args, device):
     return model
 
 
-def train_with_config(args, opts, device):
+def train_with_config(args, opts, device, mode='val'):
     """
     Train the model using the provided configuration and options.
     """
@@ -52,48 +47,73 @@ def train_with_config(args, opts, device):
     create_checkpoint_directory(opts.checkpoint)
 
     # Initialize TensorBoard writer
-    train_writer = SummaryWriter(os.path.join(opts.checkpoint, "logs"))
+    tb_writer = SummaryWriter(os.path.join(opts.checkpoint, "logs"))
 
     # Load data, model, and training utilities
-    train_loader, val_loader, test_loader = get_dataloader(args)
+    train_loader, val_loader = get_dataloader(args, mode)
     model = get_model(args, device)
     criterion = get_criterion(device)
     optimizer, scheduler = get_optimizer(args, model)
+    min_loss = np.inf
 
-    # Display model parameter count
+    # Display settings
     model_params = sum(p.numel() for p in model.parameters())
     print(f'[INFO MODEL] Trainable parameter count: {model_params} | Device: {device}')
-
-    # Initialize minimum loss
-    min_loss = np.inf
 
     # Main training loop
     for epoch in tqdm(range(args.epochs)):
         print(f'[INFO TRAIN] Epoch {epoch}.')
 
         # Initialize loss trackers
-        losses = initialize_loss_trackers()
+        losses = initialize_loss_trackers(mode)
 
         # Train for one epoch
         train_epoch(model, device, train_loader, criterion, optimizer, scheduler, losses)
 
         # Validate the model after each epoch
-        model, gt, pred = validate_epoch(model, device, val_loader, criterion, losses, mode="val")
+        model, gt, pred = evaluate_model(model, device, val_loader, criterion, losses, mode)
 
         # Log metrics to TensorBoard
-        log_metrics(train_writer, losses, optimizer, gt, pred, epoch)
+        log_metrics(tb_writer, losses, optimizer, gt, pred, epoch)
 
         # Save checkpoints
-        save_checkpoints(opts.checkpoint, model, optimizer, scheduler, epoch, losses, min_loss)
+        manage_checkpoints(opts.checkpoint, model, optimizer, scheduler, epoch, losses, min_loss)
         min_loss = min(min_loss, losses["val_RMSE_loss"].avg)
 
-    print("[INFO] Training done, performing inference on test data.")
+    print("[INFO] Training completed.")
+
+
+def test_with_config(args, opts, device, mode='test'):
+    """
+    Perform inference on the test set using the trained model.
+    """
+    print('--------------------------------------------')
+    print('######## STARTING TEST INFERENCE ########')
+    print('--------------------------------------------')
+
+    # Load test data
+    test_loader = get_dataloader(args, mode)
+    
+    # Load the model
+    model = get_model(args, device)
+    checkpoint = torch.load(os.path.join(opts.checkpoint, 'best_epoch.bin'), weights_only=True)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    epoch_loaded = checkpoint['epoch']
+    min_loss = checkpoint['min_loss']
+    print(f"[INFO MODEL] Best Model loaded | Epoch: {epoch_loaded} | Validation RMSE: {min_loss:.4f}")
 
     # Perform inference on the test set
-    model, gt, pred = validate_epoch(model, device, test_loader, criterion, losses, mode="test")
+    criterion = get_criterion(device)
+    losses = initialize_loss_trackers(mode)
+    model, gt, pred = evaluate_model(model, device, test_loader, criterion, losses, mode)
+
+    # Initialize TensorBoard writer
+    tb_writer = SummaryWriter(os.path.join(opts.checkpoint, "logs"))
 
     # Log test results to TensorBoard
-    log_test_results(train_writer, losses, gt, pred)
+    log_test_results(tb_writer, losses, gt, pred)
+
+    print("[INFO] Test inference available on TensorBoard.")
 
 
 if __name__ == "__main__": 
@@ -101,4 +121,5 @@ if __name__ == "__main__":
     set_random_seed(opts.seed)
     args = get_config(opts.config)
     train_with_config(args, opts, device)
+    test_with_config(args, opts, device)
 
